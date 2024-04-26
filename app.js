577c7ca6f00715e870da9b7dfc89aac73b5dd93e
Ian L <Ian.Luong@tclarke.co.uk>
2024-04-26 12:20:02 +0100

    Re-add env port

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('0.0.1')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
