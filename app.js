1f2a22dc08e4e055768fd37cbd2b596e344d615e
Ian L <Ian.Luong@tclarke.co.uk>
2024-05-01 09:04:00 +0100

    Add health check fail test

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.get('/version', (req, res) => {
   res.send('0.0.1')
 })
